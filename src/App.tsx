4270a56ef9a9f13c7082b885c1a83e83bafd24cd
Vladislav <vlad-krasheninni@mail.ru>
2022-02-21 17:58:18 +0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+
+function App() {
+  return (
+    <Container maxWidth="md">
+      <div className="app">Hello!</div>
+    </Container>
+  );
+}
+
+export default App;
