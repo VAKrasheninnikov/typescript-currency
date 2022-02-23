54842a1075a449d41dc40d6ff8fb6c1087ec8b74
VAKrasheninnikov <vlad-krasheninni@mail.ru>
2022-02-24 00:01:51 +0300

    fqwff

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper'
@@ -9,6 +10,12 @@ import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,17 +25,68 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+]
+
 function App() {
+
+  const [array, setArray] = React.useState([]);
+  React.useEffect(() => {
+    axios.get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD').then(({ data: { Data } })=>{
+      setArray(Data)
+    })
+  }, [])
+  console.log(array)
+
   return (
     <Container maxWidth="lg">
       <Grid container spacing={2} style={{ justifyContent: "flex-end" }}>
         <Grid item xs={8}>
-          <Item>xs=8</Item>
+          <Item style={{ backgroundColor: "#CCCCCC" }}>
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Dessert (100g serving)</TableCell>
+                    <TableCell align="right">Calories</TableCell>
+                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.calories}</TableCell>
+                      <TableCell align="right">{row.fat}</TableCell>
+                      <TableCell align="right">{row.carbs}</TableCell>
+                      <TableCell align="right">{row.protein}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Item>
         </Grid>
         <Grid item xs={4}>
           <Item>
-            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-              <div style={{display:"flex", alignItems:"center"}}>
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "stretch", justifyContent: "center" }}>
+              <div style={{ display: "flex", alignItems: "center" }}>
                 <TextField style={{ padding: '5px' }} fullWidth={true} />
                 <FormControl fullWidth>
                   <InputLabel id="demo-simple-select-label">Валюта</InputLabel>
@@ -41,7 +99,7 @@ function App() {
                   </Select>
                 </FormControl>
               </div>
-              <div style={{display:"flex", alignItems:"center"}}>
+              <div style={{ display: "flex", alignItems: "center" }}>
                 <TextField style={{ padding: '5px' }} fullWidth={true} />
                 <FormControl fullWidth>
                   <InputLabel id="demo-simple-select-label">Валюта</InputLabel>
@@ -55,7 +113,7 @@ function App() {
                 </FormControl>
               </div>
             </div>
-            <Typography style={{marginTop:"20px"}} variant="h5" gutterBottom component="div">75,36 Российский рубль</Typography>
+            <Typography style={{ marginTop: "20px" }} variant="h5" gutterBottom component="div">75,36 Российский рубль</Typography>
           </Item>
         </Grid>
       </Grid>
